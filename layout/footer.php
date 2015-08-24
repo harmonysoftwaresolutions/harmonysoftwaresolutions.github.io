dcdc6bca2eace757ed60ef18644cda46b1686571
UlicesC <ulicescarlosmr@gmail.com>
2015-08-23 22:09:00 -0400

    New files uploaded

---
+++ layout/footer.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<footer id="footer" class="midnight-blue">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    &copy; 2015 <a target="_blank" href="http://harmonyss.net46.net/" title="HARMONY Software Solutions">HARMONY Software Solutions</a>. All Rights Reserved.
+                </div>
+                <div class="col-sm-6">
+                    <ul class="pull-right">
+                        <li><a href="index.html">Inicio</a></li>
+                        <li><a href="services.html">Servicios</a></li>
+                        <li><a href="#">Faq</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer><!--/#footer-->
---
+++ layout/menu_header.php
@@ -0,0 +1,38 @@
+<?php ?>
+<header id="header">
+
+        <nav class="navbar navbar-inverse" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="logo"></a>
+                </div>
+                
+                <div class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="index.html">Inicio</a></li>
+                            <li><a href="about-us.html">Nosotros</a></li>
+                            <li><a href="services.html">Servicios</a></li>
+                            <!--<li><a href="portfolio.php">Portafolio</a></li>-->
+<!--                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Paginas <i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="blog-item.php">Blog Simple</a></li>
+                                    <li><a href="pricing.php">Precios</a></li>
+                                    <li><a href="404.php">Error</a></li>
+                                    <li><a href="shortcodes.php">Shortcodes</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="blog.php">Blog</a></li> -->
+                            <li><a href="contact-us.html">Contacto</a></li>                         
+                    </ul>
+                </div>
+            </div><!--/.container-->
+        </nav><!--/nav-->
+        
+    </header><!--/header-->
